7118eccda5f01c541aa558ba67c850bbdbd351ec
sak2 <sahmedk2@outlook.com>
2019-03-03 18:35:30 +1100

    changed color

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const bg = chrome.extension.getBackgroundPage()
     Object.keys(bg.bears).forEach(function (url) {
       const div = document.createElement('div')
-      div.d
+      div.style.color = 'red'
       div.textContent = `${url}: ${bg.bears[url]}`
       document.body.appendChild(div)
     })

b57d0f6e2af8e2f7ca06afa550213ca5042fe4ed
KnightOfTheEasternCalculus <fahad.aijaz4@gmail.com>
2019-03-03 18:26:00 +1100

    youtube tutorial

--- background.js
+++ background.js
@@ -1,16 +1,8 @@
-// Authors Saleh K (sahmedk2@outlook.com), Fahad (fahad.aijaz4@gmail.com))
+window.bears = {}
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  window.bears[request.url] = request.count
+})
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: "<all_urls>"},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.create({url: 'popup.html'})
+})
--- content.js
+++ content.js
@@ -1,1 +1,6 @@
-alert('hello')
+const re = new RegExp('bear', 'gi')
+const matches = document.documentElement.innerHTML.match(re)
+chrome.runtime.sendMessage({
+  url: window.location.href,
+  count: matches.length
+})
